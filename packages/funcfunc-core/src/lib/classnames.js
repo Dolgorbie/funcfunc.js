2c67246589f0d2d6791b008bbaee5005dcf8c42f
kuwabiraki_y <kuwabiraki_y@use-ebisu.co.jp>
2026-07-02 19:07:20 +0900

    classnames

--- packages/funcfunc-core/src/lib/classnames.js
+++ packages/funcfunc-core/src/lib/classnames.js
@@ -1,20 +1,69 @@
-export function c(...names) {
-  const acc = [];
-  _c(acc, names);
-  return acc.join(" ");
+
+export function genc({ processors = [], merge = _defaultMerge } = {}) {
+  return (...names) => {
+    const acc = [];
+    _loop(0, processors, acc, names);
+    return merge(acc);
+  }
 }
 
+function _loop(procid, processors, acc, names) {
+  const nNames = names.length;
+  Outer: for (let i = 0; i < nNames; ++i) {
+    let name = names[i];
 
-function _c(acc, names) {
-  const { length } = names;
-  for (let i = 0; i < length; ++i) {
-    const name = names[i];
-    if (name) {
+    if (Array.isArray(name)) {
+      _loop(procid, processors, acc, name);
+      continue;
+    }
+
+    const nProcessors = processors.length;
+    for (let j = procid; j < nProcessors; ++j) {
+      const proc = processors[j];
+      name = proc(name);
+      if (name === false) {
+        continue Outer;
+      }
       if (Array.isArray(name)) {
-        _c(acc, name);
-      } else {
-        acc.push(name);
+        _loop(j + 1, processors, acc, name);
+        continue Outer;
       }
     }
+
+    if (name !== false) {
+      acc.push(name);
+    }
   }
+}
+
+export const c = genc();
+
+export const clegacy = genc({ preprocess: _legacyPreprocess });
+
+export function cprops(styles) {
+  return genc({ postprocess: (prop) => prop && (styles[prop] ?? prop) });
+}
+
+export function cprefix(prefix) {
+  return genc({ postprocess: (name) => name && `${prefix}${name}` });
+}
+
+function _defaultMerge(names) {
+  return names.join(" ");
+}
+
+function _legacyPreprocess(name) {
+  if (name == null || typeof name !== "object" || Array.isArray(name)) {
+    return name;
+  }
+  const keys = Object.keys(name)
+  const acc = new Array(keys.length);
+  let i = 0;
+  for (const k of keys) {
+    if (name[k]) {
+      acc[i++] = k;
+    }
+  }
+  acc.length = i;
+  return acc;
 }
