7a37fa5bd460d2907e971313045b2a1fd30e256e
kuwabiraki_y <kuwabiraki_y@use-ebisu.co.jp>
2026-07-23 20:36:38 +0900

    queue

--- packages/funcfunc-core/src/lib/queue/inf-queue.js
+++ packages/funcfunc-core/src/lib/queue/inf-queue.js
@@ -1,28 +1,58 @@
 const _deletedMark = Symbol("deleted item");
 
 export class InfQueue {
-  constructor(sizeFactor = 2) {
-    this._initFactor = sizeFactor;
-    this._reset(sizeFactor);
+  _initFactor = 0;
+  _policy = null;
+
+  _factor = 0;
+  _mask = 0;
+  _buffer = null;
+
+  _popped = 0;
+  _added = 0;
+  _deleted = 0;
+
+  constructor(leastCapacity, policy) {
+    this._initFactor = 31 - Math.clz32(leastCapacity);
+    this._policy = policy;
+    this._reset(this._initFactor);
+  }
+
+  get capacity() {
+    return this._buffer.length;
+  }
+
+  get filled() {
+    return this._added - this._popped >>> 0;
   }
 
   get size() {
-    return (this._added - this._popped >>> 0) - this._deleted;
+    return this.filled - this._deleted;
   }
 
-  add(value) {
-    if ((this._added - this._popped >>> 0) === this._buffer.length) {
-      this._expand();
+  tryAdd(value) {
+    if (this.filled === this.capacity) {
+      return false;
     }
 
     const { _added } = this;
-
     this._buffer[_added & this._mask] = value;
     this._added = _added + 1 >>> 0;
     return true;
   }
 
-  pop() {
+  add(value) {
+    const res = this.tryAdd(value)
+    if (!res) {
+      const { _policy } = this;
+      if (_policy.onOverflow) {
+        return _policy.onOverflow(this, value);
+      }
+    }
+    return res;
+  }
+
+  tryPop() {
     const { _mask, _buffer, _popped, _added } = this;
 
     for (let i = _popped; (_added - i >>> 0) > 0; i = i + 1 >>> 0) {
@@ -33,13 +63,24 @@ export class InfQueue {
 
       if (value !== _deletedMark) {
         this._popped = i + 1 >>> 0;
-        return value;
+        return { value, success: true };
       }
 
       this._deleted -= 1;
     }
 
-    return void 0;
+    return { value: void 0, success: false };
+  }
+
+  pop() {
+    const res = this.tryPop();
+    if (!res.success) {
+      const { _policy } = this;
+      if (_policy.onUnderflow) {
+        return _policy.onUnderflow(this);
+      }
+    }
+    return res;
   }
 
   peek() {
