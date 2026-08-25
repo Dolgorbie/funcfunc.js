fd552ac6428dd4b66da4a573c88489da54873a2e
kuwabiraki_y <kuwabiraki_y@use-ebisu.co.jp>
2026-08-25 16:29:38 +0900

    cfomr

--- packages/funcfunc-react/src/lib/cform.jsx
+++ packages/funcfunc-react/src/lib/cform.jsx
@@ -1,4 +1,4 @@
-import { createContext, use, useMemo, useState } from "react";
+import { createContext, use, useMemo, useReducer, useState } from "react";
 
 export function createFormHook(options) {
   const { name = "", form: Container = "form" } = options;
@@ -11,9 +11,9 @@ export function createFormHook(options) {
 
     const [Components] = useState(() => {
       function Form({ children }) {
-        const [value, setValue] = useState();
+        const [value, dispatch] = useReducer(_reduce, fields, _initReducer);
 
-        return <SetCtx.Provider value={setValue}>
+        return <SetCtx.Provider value={dispatch}>
           <GetCtx.Provider value={value}>{
             <Container {...props}>{children}</Container>
           }</GetCtx.Provider>
@@ -22,8 +22,10 @@ export function createFormHook(options) {
       Form.displayName = `${name}Form`;
 
       function Field({ name, render, children }) {
-        const Comp = render ?? children;
         const value = use(GetCtx);
+        const dispatch = use(SetCtx);
+
+        const Comp = render ?? children;
         return useMemo(() => <Comp name={name} value={value[name]} />, [Comp, name, value[name]]);
       };
       Field.displayName = `${name}Field`;
@@ -38,31 +40,56 @@ export function createFormHook(options) {
 }
 
 
-const useForm = createFormHook({ name: "Some", form: "form" });
+function _reduce(prev, event) {
+  const { target } = event;
+  const { name, value, checked } = target;
+  const prevValue = prev[name];
 
-function SomeForm() {
-  const { Form, Field } = useForm(() => ({
-    fields: {
-      foo: {
-        value: "",
-        onChange: validateOnChange,
-        onBlur: validateOnBlur,
-      },
-      bar: {
-        value: 123,
-        onChange: validateOnChange,
-      },
-    },
-    props: {
-      onSubmit: (event, values, errors) => { }
+  if (typeof prevValue === "boolean") {
+    return prevValue === checked ? prev : { ...prev, [name]: checked };
+  }
+
+  if (Array.isArray(prevValue)) {
+    const includes = prevValue.includes(value);
+    if (checked && includes || !checked && !includes) {
+      return prev;
     }
-  }));
+    return { ...prev, [name]: checked ? [...prevValue, value] : prevValue.filter((x) => !Object.is(x, value)) };
+  }
 
-  return <Form>
-    <Field name="foo">{
-      (props) => {
-        return <input {...props} />
-      }
-    }</Field>
-  </Form>
+  return Object.is(prevValue, value) ? prev : { ...prev, [name]: value };
 }
+
+function _initReducer(fields) {
+  return Object.fromEntries(Object.keys(fields).map((key) => [key, fields[key].value]));
+}
+
+
+// const useForm = createFormHook({ name: "Some", form: "form" });
+
+// function SomeForm() {
+//   const { Form, Field } = useForm(() => ({
+//     fields: {
+//       foo: {
+//         value: "",
+//         onChange: validateOnChange,
+//         onBlur: validateOnBlur,
+//       },
+//       bar: {
+//         value: 123,
+//         onChange: validateOnChange,
+//       },
+//     },
+//     props: {
+//       onSubmit: (event, values, errors) => { }
+//     }
+//   }));
+
+//   return <Form>
+//     <Field name="foo">{
+//       (props) => {
+//         return <input {...props} />
+//       }
+//     }</Field>
+//   </Form>
+// }
