cbfdb104761e3b4e3cd44ba94db7dc8c40c5940a
Kuwabiraki Yûki <yuki61394@yahoo.co.jp>
2026-09-19 13:22:12 +0900

    richasync

--- packages/funcfunc-core/src/lib/richasync.js
+++ packages/funcfunc-core/src/lib/richasync.js
@@ -1,3 +1,5 @@
+import { DStackQueue } from "./queue/double-stack-queue";
+import { RingQueue } from "./queue/ring-queue";
 import { forEach1 } from "./sequence/array-utils";
 import { gfilter } from "./sequence/iterator-utils";
 
@@ -35,7 +37,6 @@ export function ado(func, { retry, timeout, signal: exSignal }) {
   };
 
   return run(retry?.count ?? 0);
-
 }
 
 export async function withAnySignals(signals, proc) {
@@ -57,31 +58,100 @@ export async function withAnySignals(signals, proc) {
   }
 }
 
-export function sleep(msec, { signal }) {
+export function sleep(delay, { signal }) {
   return new Promise((resolve, reject) => {
     if (signal?.aborted) {
       reject(signal.reason);
       return;
     }
 
-    const handleTimeout = () => {
-      if (signal != null) {
-        signal.removeEventListener("abort", handleAbort);
-      }
-      resolve();
-    };
-
     const handleAbort = (event) => {
       clearTimeout(timeoutId);
       reject(event.target.reason);
     };
 
-    const timeoutId = setTimeout(handleTimeout, msec);
+    const timeoutId = setTimeout(_handleSleepTimeout, delay, resolve, handleAbort);
 
-    if (signal != null) {
-      signal.addEventListener("abort", handleAbort, { once: true });
-    }
+    signal?.addEventListener("abort", handleAbort, { once: true });
   });
+}
+
+function _handleSleepTimeout(resolve, signal, handleAbort) {
+  signal?.removeEventListener("abort", handleAbort);
+  resolve();
+}
+
+export function clocks(interval, { limits, signal }) {
+  return new _ClocksAsyncIter(interval, limits, signal);
+}
+
+class _ClocksAsyncIter {
+  _resolverQueue = new DStackQueue();
+  _dateQueue = null;
+  _signal = null;
+  _done = false;
+  _intervalId = void 0;
+
+  constructor(interval, limits, signal) {
+    this._dateQueue = limits != null && Number.isFinite(limits) ? new RingQueue(limits, true) : new DStackQueue();
+    this._signal = signal;
+    this._done = signal != null && signal.aborted;
+
+    if (!this._done) {
+      this._intervalId = setInterval(this._handleInterval, interval);
+    }
+
+    signal?.addEventListener("abort", this._handleAbort, { once: true });
+  }
+
+  [Symbol.asyncIterator]() {
+    return this;
+  }
+
+  next() {
+    if (this._done) {
+      return Promise.resolve({ value: void 0, done: true });
+    }
+
+    const { _resolverQueue, _dateQueue } = this;
+
+    return new Promise((resolve) => {
+      if (_dateQueue.size > 0) {
+        resolve({ value: _dateQueue.pop(), done: false });
+        return;
+      }
+      _resolverQueue.push(resolve);
+    });
+  }
+
+  return(value) {
+    this._signal?.removeEventListener("abort", this._handleAbort);
+    this._handleAbort();
+    return Promise.resolve({ value, done: true });
+  }
+
+  _handleInterval = () => {
+    const { _resolverQueue, _dateQueue } = this;
+    if (_resolverQueue.size > 0) {
+      const resolve = _resolverQueue.pop();
+      resolve({ value: new Date(), done: false });
+      return;
+    }
+    _dateQueue.push(new Date());
+  }
+
+  _handleAbort = () => {
+    clearInterval(this._intervalId);
+    this._done = true;
+    const { _resolverQueue, _dateQueue } = this;
+    while (_resolverQueue.size > 0) {
+      const resolve = _resolverQueue.pop();
+      resolve({ value: void 0, done: true });
+    }
+    while (_dateQueue.size > 0) {
+      _dateQueue.pop();
+    }
+  }
 }
 
 export function postpone(proc, { args = [], signal }) {
@@ -96,12 +166,12 @@ export function postpone(proc, { args = [], signal }) {
       reject(event.target.reason);
     };
 
-    const timeoutId = setTimeout(_doOnPostpone, 4, resolve, proc, args, signal, handleAbort);
+    const timeoutId = setTimeout(_handlePostponeTimeout, 4, resolve, proc, args, signal, handleAbort);
     signal?.addEventListener("abort", handleAbort, { once: true });
   });
 }
 
-function _doOnPostpone(resolve, proc, args, signal, handleAbort) {
+function _handlePostponeTimeout(resolve, proc, args, signal, handleAbort) {
   signal?.removeEventListener("abort", handleAbort);
   resolve(proc(...args));
 }
